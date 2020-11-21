6451b8ffe2f40b544cb3654cb95ad25ca346844e
atriw <875241499@qq.com>
2020-11-21 14:05:00 +0800

    Add fisher plugins

--- install-fish.sh
+++ install-fish.sh
@@ -2,11 +2,5 @@
 
 [[ -f /usr/bin/fish ]] || (echo "Installing fish..." && sudo apt install -y fish && sudo chsh -s /usr/bin/fish)
 
-[[ -d ~/.local/share/omf ]] || (echo "Installing ohmyfish..." && curl -L https://get.oh-my.fish | fish)
-
-omf install https://github.com/jhillyerd/plugin-git
 [[ ! -f ~/.config/fish/config.fish ]] || rm ~/.config/fish/config.fish
-
-[[ -f ~/.cargo/bin/starship ]] || (echo "Installing starship..." && cargo install starship)
-
 ln -s $(realpath $(dirname $0))/config.fish ~/.config/fish/config.fish
